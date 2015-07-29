66c42c4ec137aaf008c566657b879973baf0081b
vivganes <vivek@vivekganesan.com>
2015-07-29 17:41:36 +0530

    Initial commit with skeleton

---
+++ src/main/java/com/dreamforceDemo/utils/RESTApiCaller.java
@@ -0,0 +1,22 @@
+package com.dreamforceDemo.utils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by vivek on 29/7/15.
+ */
+public class RESTApiCaller {
+
+    public Map<String,Object> get(String url, List<Map<String,Object>> headers){
+        return new HashMap<String, Object>();
+    }
+
+
+    public Map<String,Object> post(String url, List<Map<String,Object>> headers, String body){
+        return new HashMap<String, Object>();
+    }
+
+
+}
